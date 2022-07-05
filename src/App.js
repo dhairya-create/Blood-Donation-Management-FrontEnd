910ac2771bbb0624f33ea3dee202fe1137584974
Jenish Shah <jenishshah1604@gmail.com>
2022-07-05 09:15:22 +0530

    register page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menu';
+import { PLACES } from './shared/places';
+import Greet from './components/greet';
+import Footer from './components/Footer';
+import Register from './components/Register'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {					
+  
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			places: PLACES
+		};
+	}
+	
+	render(){
+		return (
+							
+      				<BrowserRouter>
+					<Routes>
+					<Route path='/register' element={<Register />} />
+					<Route path='/' element={
+						<>
+							<div className='App'>
+						<Navbar color="primary"></Navbar>
+						<div className="container" id="brand">
+						<NavbarBrand>Daiict Blood bank</NavbarBrand>
+						</div>
+					<h1>Blood Bank Management System</h1>
+					<Menu places={this.state.places}>
+         	 </Menu>
+			  <Footer/>
+   
+   			</div>
+						</>
+					}/>
+					</Routes>
+			</BrowserRouter>			
+     
+		);
+	}
+  
+
+
 }
 
 export default App;
